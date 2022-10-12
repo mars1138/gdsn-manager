06a2e0d6580599186a3d214ec9a612965cffbddd
Mario Delgado <mdelgado1138@hotmail.com>
2022-10-12 16:13:39 -0700

    misc edit

--- src/services/ServicesPage.js
+++ src/services/ServicesPage.js
@@ -3,6 +3,8 @@ import React from 'react';
 import TabComponent from '../shared/components/TabComponent/TabComponent';
 import Card from '../shared/UIElements/Card';
 import Section from '../shared/components/layout/Section';
+import Slider from '../shared/components/Slider/Slider';
+
 import classes from './ServicesPage.module.css';
 
 import img1 from '../assets/pexels-antonius-natan-11835350.jpg';
@@ -45,24 +47,64 @@ const tabContent = [
   },
 ];
 
+const sliderGallery = [
+  {
+    imgUrl: '/media/pexels-tiger-lily-4483942.jpg',
+  },
+  {
+    imgUrl: '/media/pexels-fauxels-3183197.jpg',
+  },
+  {
+    imgUrl: '/media/pexels-fauxels-3184418.jpg',
+  },
+];
+
+const sliderTestimonial = [
+  {
+    author: 'Author Name 1',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
+    imgUrl: '/media/pexels-tiger-lily-4483942.jpg',
+  },
+  {
+    author: 'Author Name 2',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
+    imgUrl: '/media/pexels-fauxels-3183197.jpg',
+  },
+  {
+    author: 'Author Name 3',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
+    imgUrl: '/media/pexels-fauxels-3184418.jpg',
+  },
+];
+
 const ServicesPage = () => {
   return (
     <React.Fragment>
       <Section>
         <h1>Services</h1>
+        <TabComponent>{tabContent}</TabComponent>
+      </Section>
+      <Section trigger="0.1">
+        <Slider type="gallery" content={sliderGallery} />
+        <Slider type="testimonial" content={sliderTestimonial} />
+      </Section>
+      {/* <Section>
         <div className={classes.cards}>
           <div className={classes.first}>
             <Card>
               <h2>CARD CONTENT</h2>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
                 tempora provident explicabo inventore pariatur, nobis cumque a
                 culpa quod, alias voluptate veniam quae qui ut at, nostrum est
                 porro enim.
               </p>
             </Card>
-          </div>
-          <div className={classes.second}>
+            </div>
+            <div className={classes.second}>
             <Card>
               <h2>CARD CONTENT</h2>
               <p>
@@ -72,34 +114,31 @@ const ServicesPage = () => {
                 porro enim.
               </p>
             </Card>
-          </div>
-          <div className={classes.third}>
+            </div>
+            <div className={classes.third}>
             <Card>
-              <h2>CARD CONTENT</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            <h2>CARD CONTENT</h2>
+            <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
                 tempora provident explicabo inventore pariatur, nobis cumque a
                 culpa quod, alias voluptate veniam quae qui ut at, nostrum est
                 porro enim.
-              </p>
-            </Card>
-          </div>
-          <div className={classes.fourth}>
+                </p>
+                </Card>
+                </div>
+                <div className={classes.fourth}>
             <Card>
-              <h2>CARD CONTENT</h2>
+            <h2>CARD CONTENT</h2>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
                 tempora provident explicabo inventore pariatur, nobis cumque a
                 culpa quod, alias voluptate veniam quae qui ut at, nostrum est
                 porro enim.
-              </p>
-            </Card>
+                </p>
+                </Card>
           </div>
         </div>
-      </Section>
-      <Section>
-        <TabComponent>{tabContent}</TabComponent>
-      </Section>
+      </Section> */}
     </React.Fragment>
   );
 };
