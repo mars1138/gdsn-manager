dd97bd9eef9674e6d8ed9e9fce3ece57b129475d
Mario Delgado <mdelgado1138@hotmail.com>
2022-12-13 17:00:21 -0800

    connect auth.js to backend

--- src/user/Auth.js
+++ src/user/Auth.js
@@ -6,7 +6,10 @@ import Section from '../shared/components/layout/Section';
 import Card from '../shared/UIElements/Card';
 import FormInput from '../shared/components/FormElements/FormInput';
 import Button from '../shared/UIElements/Button';
+import Modal from '../shared/UIElements/Modal';
+import LoadingSpinner from '../shared/UIElements/LoadingSpinner';
 import { useForm } from '../shared/components/hooks/form-hook';
+import { useHttpClient } from '../shared/components/hooks/http-hook';
 import { authActions } from '../store/auth-slice';
 import {
   VALIDATOR_EMAIL,
@@ -19,6 +22,8 @@ const Auth = () => {
   const isAuth = useSelector(state => state.auth.isAuthenticated);
   const dispatch = useDispatch();
   const history = useHistory();
+  const { isSubmitting, error, sendRequest, clearError } = useHttpClient();
+  const [didSubmit, setDidSubmit] = useState(false);
   const [isLoginMode, setIsLoginMode] = useState(true);
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -68,9 +73,40 @@ const Auth = () => {
     setIsLoginMode(previousMode => !previousMode);
   };
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async event => {
     event.preventDefault();
-    console.log(formState.inputs);
+
+    if (isLoginMode) {
+      try {
+        await sendRequest(
+          'http://localhost:5000/api/users/login',
+          'POST',
+          JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            'Content-Type': 'application/json',
+          },
+        );
+      } catch (err) {}
+    } else {
+      try {
+        await sendRequest(
+          'http://localhost:5000/api/users/signup',
+          'POST',
+          JSON.stringify({
+            name: formState.inputs.name.value,
+            company: formState.inputs.company.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            'Content-Type': 'application/json',
+          },
+        );
+      } catch (err) {}
+    }
     dispatch(authActions.login());
     history.push('/products');
   };
@@ -79,8 +115,16 @@ const Auth = () => {
     <Section>
       <div className={classes['auth-container']}>
         <Card width="40">
+          <Modal
+            show={error === undefined || null ? false : true}
+            msgHeader="Error creating product"
+            onClear={clearError}
+          >
+            {`${error ? error : 'An unknown error occurred'}`}
+          </Modal>
           <h3>Please Login</h3>
           <form onSubmit={authSubmitHandler}>
+            {isSubmitting && <LoadingSpinner />}
             {!isLoginMode && (
               <>
                 <FormInput
