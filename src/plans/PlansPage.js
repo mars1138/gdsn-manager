b258e703aedf55e94fb356f45c78c72627d7315d
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-02-01 21:27:15 -0800

    update plans slider

--- src/plans/PlansPage.js
+++ src/plans/PlansPage.js
@@ -1,69 +1,38 @@
 import React from 'react';
 
 import Hero from '../shared/components/layout/Hero';
-import TabComponent from '../shared/components/TabComponent/TabComponent';
+// import TabComponent from '../shared/components/TabComponent/TabComponent';
 import Section from '../shared/components/layout/Section';
-// import Slider from '../shared/components/Slider/Slider';
+import Slider from '../shared/components/Slider/Slider';
 import PlanCard from './PlanCard';
 import { planCards } from '../assets/data/plansData';
 
 import classes from './PlansPage.module.css';
 
-import img1 from '../assets/pexels-tiger-lily-4483942.jpg';
-import img2 from '../assets/pexels-fauxels-3183197.jpg';
-import img3 from '../assets/pexels-fauxels-3184418.jpg';
+import slide1 from '../assets/media/home/pexels-tiger-lily-4483942-min.jpg';
+import slide2 from '../assets/media/home/pexels-monstera-6289028-min.jpg';
+import slide3 from '../assets/media/home/pexels-burak-the-weekender-186461-min.jpg';
 
-// import cust1 from '../assets/media/plans/2.jpg';
-// import cust2 from '../assets/media/plans/51.jpg';
-// import cust3 from '../assets/media/plans/59.jpg';
-
-const tabContent = [
+const sliderHome = [
   {
-    id: 1,
-    tabTitle: 'Basic',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    contentHeading: 'Tab 1 Heading',
-    imgUrl: img1,
+    title: 'Product Management',
+    text:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
+    imgUrl: slide1,
   },
   {
-    id: 2,
-    tabTitle: 'Pro',
-    content:
-      'Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    contentHeading: 'Tab 2 Heading',
-    imgUrl: img2,
+    title: 'E-commerce Support',
+    text:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
+    imgUrl: slide2,
   },
   {
-    id: 3,
-    tabTitle: 'Expert',
-    content:
-      'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    contentHeading: 'Tab 3 Heading',
-    imgUrl: img3,
+    title: 'Powerful Analytics',
+    text:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
+    imgUrl: slide3,
   },
 ];
-
-// const sliderTestimonial = [
-//   {
-//     author: 'Author Name 1',
-//     quote:
-//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
-//     imgUrl: cust1,
-//   },
-//   {
-//     author: 'Author Name 2',
-//     quote:
-//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
-//     imgUrl: cust2,
-//   },
-//   {
-//     author: 'Author Name 3',
-//     quote:
-//       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus at error accusantium mollitia.  Voluptas qui, aperiam magnam quae exercitationem modi eveniet quia vel quisquam voluptatem quidem nostrum amet dolores quis?',
-//     imgUrl: cust3,
-//   },
-// ];
 
 const heroTitle = 'Pricing Plans For Your Needs';
 const heroText =
@@ -85,11 +54,9 @@ const PlansPage = () => {
         <div className={classes.planCards}>{plansArray}</div>
       </Section>
       <Section>
-        <TabComponent>{tabContent}</TabComponent>
+        <h2>Plans That Suit Any Company's Mission</h2>
+        <Slider type="description" content={sliderHome} />
       </Section>
-      {/* <Section>
-        <Slider type="testimonial" content={sliderTestimonial} />
-      </Section> */}
     </React.Fragment>
   );
 };
